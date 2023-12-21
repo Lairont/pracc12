ea66fb683fd1aa4f53a1fcbc4c3084d7ba10ef99
Никита <153456515+Lairont@users.noreply.github.com>
2023-12-22 00:15:08 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Задача 1.Используя случайное число в диапазоне от 1 до 7, вывести на экран название дня недели.
+            //Random rnd = new Random();
+            //int day = rnd.Next(-10, 10);
+            //Console.WriteLine(day);
+            //switch (day)
+            //{
+            //    case 1: Console.WriteLine("Понедельник"); break;
+            //    case 2: Console.WriteLine("Вторник"); break;
+            //    case 3: Console.WriteLine("Среда"); break;
+            //    case 4: Console.WriteLine("Четверг"); break;
+            //    case 5: Console.WriteLine("Пятница"); break;
+            //    case 6: Console.WriteLine("Суботта"); break;
+            //    case 7: Console.WriteLine("Воскресенье"); break;
+            //    default: Console.WriteLine("Ошибка"); break;
+            //}
+            //Console.ReadLine();
+
+            //Задание 2 
+            //    Console.Write("Первое число: ");
+            //    int x = int.Parse(Console.ReadLine());
+
+            //    Console.Write("Второе число: ");
+            //    int y = int.Parse(Console.ReadLine());
+
+            //    if (x % 2==0&&y%2==0)
+            //    {
+            //        x /= 2;
+            //        y /= 2;
+            //        Console.WriteLine($"{x}\n{y}");
+            //    }
+            //    else if (x%2==0||y%2==0)
+            //    {
+            //        x *= 2;
+            //        y *= 2;
+            //        Console.WriteLine($"{x}\n{y}");
+            //    }
+            //    else
+            //    {
+            //        x += 1;
+            //        y += 1;
+            //        Console.WriteLine($"{x}\n{y}");
+            //    }
+
+            //    Console.ReadLine();
+
+            //задание 3
+            //Console.Write("Введите количество баллов: ");
+            //int ball = int.Parse(Console.ReadLine());
+            //if (ball>=90&&ball<=100)
+            //{
+            //    Console.WriteLine("Отлично");
+            //}
+            //else if (ball>=70&&ball<=89)
+            //{
+            //    Console.WriteLine("Хорошо");
+            //}
+            //else if (ball>=50&&ball<=69)
+            //{
+            //    Console.WriteLine("Удовлетворительно");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Неудовлетворительно");
+            //}
+            //Console.ReadLine();
+        }
+    }
+}
